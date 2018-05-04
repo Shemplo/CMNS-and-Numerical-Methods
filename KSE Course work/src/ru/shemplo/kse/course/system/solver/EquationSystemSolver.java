2a41bb8840d8868af37b6a782ca46367ad39df32
Shemplo <shemplo@outlook.com>
2018-05-04 19:40:32 +0300

    Derivative and smth other

--- KSE Course work/src/ru/shemplo/kse/course/system/solver/EquationSystemSolver.java
+++ KSE Course work/src/ru/shemplo/kse/course/system/solver/EquationSystemSolver.java
@@ -6,4 +6,14 @@ public interface EquationSystemSolver {
 
 	public double [] solve (EquationSystem system) throws IllegalArgumentException;
 	
+	default
+	public double majorValue (double [] vector) {
+		double value = 0;
+		for (int i = 0; i < vector.length; i++) {
+			value = Math.max (value, vector [i]);
+		}
+		
+		return value;
+	}
+	
 }
--- KSE Course work/src/ru/shemplo/kse/course/system/solver/UniversalSolver.java
+++ KSE Course work/src/ru/shemplo/kse/course/system/solver/UniversalSolver.java
@@ -26,15 +26,54 @@ public class UniversalSolver implements EquationSystemSolver {
 		
 		for (int i = 0; i < size; i++) {
 			double [] dx = findGradient (system, result);
-			double min = 1;
+			double min = localMin (system, dx, result);
 			for (int j = 0; j < size; j++) {
 				result [i] += min * dx [i];
 			}
 			
-			// ...
+			if (majorValue (dx) < Run.PRECISION) {
+				break; // Req. precision reached
+			}
 		}
 		
 		return result;
+	}
+	
+	private double vectorMismatch (EquationSystem system, double [] vector) {
+		double mis = 0;
+		for (int i = 0; i < system.getSize (); i++) {
+			double value = system.getEquation (i)
+								 .evaluate (vector);
+			mis += value * value;
+		}
+		
+		return mis;
+	}
+	
+	private double vectorMismatch (EquationSystem system, double [] vector, 
+									double [] gradient, double coft) {
+		double [] tmp = new double [system.getSize ()];
+		for (int i = 0; i < system.getSize (); i++) {
+			tmp [i] = vector [i] + gradient [i] * coft;
+		}
+		
+		return vectorMismatch (system, tmp);
+	}
+	
+	private double localMin (EquationSystem system, double [] input, double [] gradient) {
+		double mis = vectorMismatch (system, input);
+		mis = Math.min (mis, vectorMismatch (system, input, gradient, 1));
+		
+		double rad = 1, dr;
+		do {
+			rad *= 2;
+			dr = vectorMismatch (system, input, gradient, rad);
+			mis = Math.min (mis, dr);
+		} while (dr <= mis);
+		
+		// TODO: ... 
+		
+		return 0;
 	}
 	
 	private double [] findGradient (EquationSystem system, double [] input) {
