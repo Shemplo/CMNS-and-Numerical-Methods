2ba1e3cdb3b3887dc387a2d5174a8d1a4a3d4f35
Egor Makarenko <egormkn@yandex.ru>
2018-09-17 11:52:44 +0300

    Fixed predictor-corrector

--- DM Lorenz system (attractor)/adamssolver.cpp
+++ DM Lorenz system (attractor)/adamssolver.cpp
@@ -28,14 +28,12 @@ std::vector<double> *AdamsSolver::solve(double time) {
     while (currentTime < time - dt) {
 
         double x_pred = xk[n] + (dt / 24) * (55 * x(xk[n], yk[n], zk[n]) - 59 * x(xk[n-1], yk[n-1], zk[n-1]) + 37 * x(xk[n-2], yk[n-2], zk[n-2]) - 9 * x(xk[n-3], yk[n-3], zk[n-3]));
-        double x_corr = xk[n] + (dt / 24) * (9 * x(x_pred, yk[n], zk[n]) + 19 * x(xk[n], yk[n], zk[n]) - 5 * x(xk[n-1], yk[n-1], zk[n-1]) + x(xk[n-2], yk[n-2], zk[n-2]));
-
         double y_pred = yk[n] + (dt / 24) * (55 * y(xk[n], yk[n], zk[n]) - 59 * y(xk[n-1], yk[n-1], zk[n-1]) + 37 * y(xk[n-2], yk[n-2], zk[n-2]) - 9 * y(xk[n-3], yk[n-3], zk[n-3]));
-        double y_corr = yk[n] + (dt / 24) * (9 * y(xk[n], y_pred, zk[n]) + 19 * y(xk[n], yk[n], zk[n]) - 5 * y(xk[n-1], yk[n-1], zk[n-1]) + y(xk[n-2], yk[n-2], zk[n-2]));
-
         double z_pred = zk[n] + (dt / 24) * (55 * z(xk[n], yk[n], zk[n]) - 59 * z(xk[n-1], yk[n-1], zk[n-1]) + 37 * z(xk[n-2], yk[n-2], zk[n-2]) - 9 * z(xk[n-3], yk[n-3], zk[n-3]));
-        double z_corr = zk[n] + (dt / 24) * (9 * z(xk[n], yk[n], z_pred) + 19 * z(xk[n], yk[n], zk[n]) - 5 * z(xk[n-1], yk[n-1], zk[n-1]) + z(xk[n-2], yk[n-2], zk[n-2]));
 
+        double x_corr = xk[n] + (dt / 24) * (9 * x(x_pred, y_pred, z_pred) + 19 * x(xk[n], yk[n], zk[n]) - 5 * x(xk[n-1], yk[n-1], zk[n-1]) + x(xk[n-2], yk[n-2], zk[n-2]));
+        double y_corr = yk[n] + (dt / 24) * (9 * y(x_pred, y_pred, z_pred) + 19 * y(xk[n], yk[n], zk[n]) - 5 * y(xk[n-1], yk[n-1], zk[n-1]) + y(xk[n-2], yk[n-2], zk[n-2]));
+        double z_corr = zk[n] + (dt / 24) * (9 * z(x_pred, y_pred, z_pred) + 19 * z(xk[n], yk[n], zk[n]) - 5 * z(xk[n-1], yk[n-1], zk[n-1]) + z(xk[n-2], yk[n-2], zk[n-2]));
 
         triple[0].push_back(x_corr);
         triple[1].push_back(y_corr);
