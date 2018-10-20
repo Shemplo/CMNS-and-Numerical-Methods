27b922962dd3eba948de990351a8df86784b53e3
Shemplo <shemplo@outlook.com>
2018-10-20 20:21:51 +0300

    Migration to Java

---
+++ DM Difference schemes/src/ru/shemplo/dm/difschemes/RunDifferenceSchemes.java
@@ -0,0 +1,79 @@
+package ru.shemplo.dm.difschemes;
+
+import static java.lang.ClassLoader.*;
+
+import java.io.IOException;
+
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class RunDifferenceSchemes extends Application {
+
+	private static Scene scene;
+	
+	public static enum View {
+		
+		U ("paramU"), K ("paramK"), dT ("paramDT"), dX ("paramDX"),
+		SIMULATE ("simulate"), AUTO_PLAY ("autoPlay");
+		
+		private final String ID;
+		
+		private View (String id) {
+			this.ID = id;
+		}
+		
+		@SuppressWarnings ("unchecked")
+		public <R extends Node> R get () {
+			return (R) scene.lookup ("#" + ID);
+		}
+		
+	}
+	
+	public static void main (String ... args) {
+		try {
+			Parent root = FXMLLoader.load (getSystemResource ("fxml/main.fxml"));
+			RunDifferenceSchemes.scene = new Scene (root);
+			
+			URL styles = getSystemResource ("css/main.css");
+			root.getStylesheets ().add (styles.toExternalForm ());
+		} catch (IOException ioe) {
+			System.err.println ("Failed to initialize main scene");
+			System.err.println ("Reason: " + ioe);
+			System.exit (1);
+		}
+		
+		launch (args);
+	}
+
+	@Override
+	public void start (Stage stage) throws Exception {
+		stage.setTitle ("Difference schemes");
+		stage.setResizable (false);
+		stage.setScene (scene);
+		stage.show ();
+		
+		// INITIALIZATION OF GUI CHOICE BOXES //
+		
+		// ... see later ...
+		
+		// INITIALIZATION OF GUI HANDLERS //
+		
+		Button simulate = View.SIMULATE.get ();
+		simulate.setOnMouseClicked (me -> {
+			System.out.println ("Start new simulation");
+		});
+		
+		Button autoPlay = View.AUTO_PLAY.get ();
+		autoPlay.setOnMouseClicked (me -> {
+			System.out.println ("Start auto play");
+		});
+	}
+	
+}
---
+++ DM Difference schemes/src/ru/shemplo/dm/difschemes/SimulationPreset.java
@@ -0,0 +1,12 @@
+package ru.shemplo.dm.difschemes;
+
+
+public enum SimulationPreset {
+	
+	J4F ("just for fun");
+	
+	private SimulationPreset (String name) {
+		
+	}
+	
+}
