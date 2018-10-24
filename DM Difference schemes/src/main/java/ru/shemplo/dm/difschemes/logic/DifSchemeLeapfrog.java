c5b6b86cb5d0d14cd64b3acd4989c151a7bdfd7c
Shemplo <shemplo@outlook.com>
2018-10-24 07:32:09 +0300

    Leapfrog method done

--- DM Difference schemes/src/main/java/ru/shemplo/dm/difschemes/logic/DifSchemeLeapfrog.java
+++ DM Difference schemes/src/main/java/ru/shemplo/dm/difschemes/logic/DifSchemeLeapfrog.java
@@ -12,13 +12,24 @@ public class DifSchemeLeapfrog extends AbsDifferenceScheme {
 
 	@Override
 	protected double [] doUnexistingStep (int step) {
-		double [][] previous = {getTimeLayer (step - 2), getTimeLayer (step - 1)};
+		double [] zeroLayaer = getTimeLayer (0), profile = new double [zeroLayaer.length];
+		double left = zeroLayaer [0], right = zeroLayaer [zeroLayaer.length - 1];
+		profile [0] = left; profile [profile.length - 1] = right;
 		
-		double [] profile = new double [previous.length];
-		for (int i = 1; i < profile.length - 1; i++) {
-			profile [i] = previous [0][i] 
-						- 0.5 * (previous [1][i + 1] - previous [1][i - 1]) 
-						+ 2 * 0;
+		if (step == 1) {
+			for (int i = 0; i < profile.length - 1; i++) {
+				profile [i] = zeroLayaer [i] * (1 - 2 * R)
+							+ (R - S / 2) * zeroLayaer [i + 1]
+							+ (R + S / 2) * zeroLayaer [i - 1];
+			}
+		} else if (step > 1) {			
+			double [][] previous = {getTimeLayer (step - 2), getTimeLayer (step - 1)};
+			
+			for (int i = 1; i < profile.length - 1; i++) {
+				profile [i] = previous [0][i] - previous [1][i] * 4 * R
+							+ previous [1][i + 1] * (2 * R - S)
+							+ previous [1][i - 1] * (2 * R + S);
+			}
 		}
 		
 		return profile;
