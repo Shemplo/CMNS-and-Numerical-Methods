a7d7d971ef97043907153ec099737c2496b6cc53
Egor Makarenko <egormkn@yandex.ru>
2019-01-22 04:24:31 +0300

    Add layout and css

--- DM Course work/src/main/java/ru/shemplo/dm/course/Functions.java
+++ DM Course work/src/main/java/ru/shemplo/dm/course/Functions.java
@@ -1,25 +1,25 @@
 package ru.shemplo.dm.course;
 
-import static java.lang.Math.*;
-import static ru.shemplo.dm.course.Constants.*;
-import static ru.shemplo.dm.course.Constants.E;
-
 import java.util.function.BiFunction;
-import java.util.function.Function;
+
+import static java.lang.Math.exp;
+import static java.lang.Math.pow;
+import static ru.shemplo.dm.course.Constants.*;
 
 public class Functions {
-    
-    public static final Function <Double, Double>
-        EXP = T -> exp (-E / (R * T));
-    
-    public static final BiFunction <Double, Double, Double> 
-        W = (a, T) -> -K * pow (a, ALPHA) * EXP.apply (T);
-    
-    public static final BiFunction <Double, Double, Double>
-        dW_dA = (a, T) -> -ALPHA * K * pow (a, ALPHA - 1) * EXP.apply (T);
-        
-    public static final BiFunction <Double, Double, Double>
-        dW_dT = (a, T) -> -E * K * pow (a, ALPHA) * EXP.apply (T) 
-                        / (R * pow (T, 2));
-        
+
+    /**
+     * Speed of reaction
+     *
+     * Скорость реакции как функция концентрации и температуры
+     */
+    public static final BiFunction<Double, Double, Double>
+            W = (X, T) -> -K * pow(X, ALPHA) * exp(-E / (R * T));
+
+    public static final BiFunction<Double, Double, Double>
+            dW_dA = (a, T) -> -ALPHA * K * pow(a, ALPHA - 1) * exp(-E / (R * T));
+
+    public static final BiFunction<Double, Double, Double>
+            dW_dT = (a, T) -> -E * K * pow(a, ALPHA) * exp(-E / (R * T)) / (R * pow(T, 2));
+
 }
